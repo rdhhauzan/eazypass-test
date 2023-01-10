47701772da3b609ba6a8979e6cfd42eab1e18edb
Ridho Hauzan <dotcomhomo@gmail.com>
2023-01-10 14:42:20 +0700

    upload and add csv content to json success

--- controller/controller.js
+++ controller/controller.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
 const csv = require("fast-csv");
 const axios = require("axios");
+const dbUrl = "http://localhost:4040";
 
 class Controller {
-  static async uploadCsv(req, res) {
+  static uploadCsv(req, res) {
     try {
       if (!req.file) {
         return res.status(400).send({ message: "Please upload a CSV File!" });
@@ -23,12 +24,24 @@ class Controller {
           ticket.push(row);
         })
         .on("end", () => {
-          if (ticket.length == 1) {
-            console.log("satu");
-          } else {
-            console.log("lebih dari satu");
-          }
-          console.log(ticket.length);
+          ticket.forEach((el, idx) => {
+            el["Package Event"] = el["Package Event"].replace(/\s/g, "");
+            let type;
+            if (el["Package Event"] == "Day1") {
+              type = 1;
+            } else if (el["Package Event"] == "Day2") {
+              type = 2;
+            } else if (el["Package Event"] == "2DayPass") {
+              type = 3;
+            }
+            // console.log(el["Package Event"]);
+            axios.post(`${dbUrl}/tickets`, {
+              Ticket_id: el["Ticket Id"],
+              Package_event: el["Package Event"],
+              Name: el["Name"],
+              typeId: type,
+            });
+          });
           res.status(200).send(ticket);
         });
     } catch (error) {
